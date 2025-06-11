f8d4a58e5b89c5419f00238b83982108a647ef1e
Ensild <ensildhallanjaku2004@gmail.com>
2025-06-11 12:24:10 +0200

    config js update

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,5 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api'; 
+
+const API_BASE_URL = "https://web-development-assignment-f8rk.onrender.com";
+
+export default API_BASE_URL;
